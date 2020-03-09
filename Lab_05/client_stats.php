089d099d43c081d0d94571f264e3f934763a7ca4
Olexandr Korotetskyi <ayrum1158@gmail.com>
2020-03-09 23:01:13 +0200

    Lab 05: done 1 of 3 tasks
    Signed-off-by: Olexandr Korotetskyi <ayrum1158@gmail.com>

---
+++ Lab_05/client_stats.php
@@ -0,0 +1,41 @@
+<?php
+    $db_driver="mysql";
+    $host="localhost";
+    $database="itech_lb1";
+    $dsn = "$db_driver:host=$host;dbname=$database";
+
+    $username="root";
+    $password="";
+    $options = array(PDO::ATTR_PERSISTENT => true, PDO:: MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+
+    try
+    {
+        $dbh = new PDO($dsn, $username, $password, $options);
+    }
+    catch (PDOException $e)
+    {
+        echo "Error!: " . $e->getMessage() . "<br/>";
+        die();
+    }
+
+//статистика работы клиента это: общее время проведенное в сети, а также входящий и выходящий трафик
+
+    $cmdSQL_Select =
+    "SELECT name, sum(in_traffic), sum(out_traffic),
+     SEC_TO_TIME(SUM(TIME_TO_SEC(TIMEDIFF(stop,start)))) AS Time
+    FROM client JOIN seanse ON client.ID_Client = FID_Client
+        WHERE client.Name = :username";
+
+    $sth = $dbh->prepare($cmdSQL_Select);
+
+    $sth->execute(array(':username' => $_POST['username']));
+
+    $result = $sth->fetch(PDO::FETCH_NUM);
+
+    foreach($result as $oneOFResults)
+    {
+        echo "$oneOFResults<br>";
+    }
+
+    $dbh=null;
+?>
