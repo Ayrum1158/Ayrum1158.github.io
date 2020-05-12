b49baaf384332e3bbe4fbde1aa2535f7b7fc07c4
Ayrum1158 <ayrum1158@gmail.com>
2020-05-12 14:30:06 +0300

    Lab_08: fixed double click on send button no focus on sessage
    Signed-off-by: Ayrum1158 <ayrum1158@gmail.com>

--- Lab_08/nodejs/public/chat.js
+++ Lab_08/nodejs/public/chat.js
@@ -9,21 +9,22 @@ var output = document.getElementById('output');
 var msgWindow = document.getElementById('chat-window');
 
 //Emit events
-
 btn.addEventListener('click', function() {
+    message.focus();
     if(message.value === "")
+        return;
+    if(nickname.value === "")
         return;
         
     socket.emit('chat', {
         message: message.value,
         nickname: nickname.value
     });
+    
     message.value = "";
-    message.focus();
 });
 
 //Listen for events
-
 socket.on('chat', function(data) {
     output.innerHTML += '<p><strong>'+ data.nickname +': </strong>' + data.message + '</p>';
 
